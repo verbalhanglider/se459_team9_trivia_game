473e807b8ab9026de1368f1d92d5bf7fd07f21b5
peilin82 <peilin@live.com>
2017-10-27 14:42:07 -0500

    Add files via upload

---
+++ teams/InstructionsPage.java
@@ -0,0 +1,90 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+
+public class InstructionsPage extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			InstructionsPage dialog = new InstructionsPage();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public InstructionsPage() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblInstructions = new JLabel("INSTRUCTIONS");
+			lblInstructions.setFont(new Font("Lucida Grande", Font.BOLD, 40));
+			lblInstructions.setHorizontalAlignment(SwingConstants.CENTER);
+			lblInstructions.setBounds(70, 6, 325, 48);
+			contentPanel.add(lblInstructions);
+		}
+		
+		JTextArea txtrTest = new JTextArea();
+		txtrTest.setText("Rule 1:\n\nRule 2:\n\nRule 3:\n\nRule 4:\n\nRule 5:\n\nRule 6:\n\nRule 7:\n……..");
+		txtrTest.setBounds(17, 52, 415, 181);
+		txtrTest.setEditable(false);
+		txtrTest.setAutoscrolls(true);
+		
+		
+		contentPanel.add(txtrTest);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
---
+++ teams/StartPage.java
@@ -0,0 +1,83 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Color;
+
+public class StartPage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartPage window = new StartPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StartPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnInstructions = new JButton("INSTRUCTIONS");
+		btnInstructions.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				InstructionsPage instructionsPage = new InstructionsPage();
+				instructionsPage.setVisible(true);
+			}
+		});
+		btnInstructions.setBounds(77, 222, 270, 29);
+		frame.getContentPane().add(btnInstructions);
+		
+		JButton btnPlay = new JButton("PLAY");
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GameSettingPage gameSetting = new GameSettingPage();
+				gameSetting.setVisible(true);
+				frame.setVisible(false);
+			}
+		});
+		btnPlay.setBounds(77, 189, 270, 29);
+		frame.getContentPane().add(btnPlay);
+		
+		JLabel lblTrivia = new JLabel("TRIVIA");
+		lblTrivia.setFont(new Font("Lucida Grande", Font.BOLD, 40));
+		lblTrivia.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTrivia.setBounds(77, 6, 270, 39);
+		frame.getContentPane().add(lblTrivia);
+		
+		JLabel lblAnAgileGame = new JLabel("An agile game");
+		lblAnAgileGame.setForeground(Color.BLACK);
+		lblAnAgileGame.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAnAgileGame.setBounds(166, 58, 100, 16);
+		frame.getContentPane().add(lblAnAgileGame);
+	}
+}
