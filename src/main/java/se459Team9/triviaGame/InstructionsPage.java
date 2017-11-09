9d20993dccda28713ff383e00af0b52e53289dae
iiliev84 <iiliev84@gmail.com>
2017-11-08 22:33:08 -0600

    adding instructions to the InstructionsPage

--- src/main/java/se459Team9/triviaGame/InstructionsPage.java
+++ src/main/java/se459Team9/triviaGame/InstructionsPage.java
@@ -40,7 +40,7 @@ public class InstructionsPage extends JDialog {
 	 * Create the dialog.
 	 */
 	public InstructionsPage() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 688, 622);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -54,8 +54,30 @@ public class InstructionsPage extends JDialog {
 		}
 		
 		JTextArea txtrTest = new JTextArea();
-		txtrTest.setText("Rule 1:\n\nRule 2:\n\nRule 3:\n\nRule 4:\n\nRule 5:\n\nRule 6:\n\nRule 7:\n……..");
-		txtrTest.setBounds(17, 52, 415, 181);
+		txtrTest.setText("Rule 1: Teams are composed of pairs.\n"
+				+ "Rule 2: The trivia cards are placed in a single stack and used by all teams.\n"
+				+ "Rule 3: A round last 2 minutes multiplied by the number of teams.\n"
+				+ "Rule 4: During a round, one player from a team pulls trivia cards from the \n"
+				+ "stack and after reading the brief description tries to get their teammate \n"
+				+ "to guess the agile term on that card by giving the teammate clues.\n"
+				+ "Rule 5: Reader is not allowed to use any agile words in clues from the \n"
+				+ "prohibited list. \n"
+				+ "Rule 6: When the guesser guesses correctly, the reader puts the card in the \n"
+				+ "team’s guessed cards and draws a new card. When the guesser guesses \n"
+				+ "incorrectly the reader puts the card in the game “wrong guess” pool \n"
+				+ "Rule 7: A reader can skip a card for any reason, but a skipped card counts \n"
+				+ "as a wrong answer. \n"
+				+ "Rule 8: If a reader uses an agile word in a clue from the prohibited list, \n"
+				+ "the reader must put the card in the “wrong guess” pool.\n"
+				+ "Rule 9: The reader keeps pulling cards from the trivia stack until the time \n"
+				+ "is up.\n"
+				+ "Rule 10: Once the time is up, the next team has a turn. This goes on until \n"
+				+ "all teams have had an opportunity to guess. Once it comes back to the first \n"
+				+ "team, the player who was guessing now has an opportunity to read and the \n"
+				+ "player who was reading has to be a guesser. Do this for every team. This is \n"
+				+ "the start of a new round.\n"
+				+ "Rule 11: A game lasts eight rounds.");
+		txtrTest.setBounds(17, 52, 620, 418);
 		txtrTest.setEditable(false);
 		txtrTest.setAutoscrolls(true);
 		
